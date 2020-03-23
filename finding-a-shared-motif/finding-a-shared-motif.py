b8d122d40630a6f8305a8a3d1dbe14ea010a7db9
lobvh <71gordi@gmail.com>
2020-03-23 18:21:24 +0100

    Resolving problem 'Finding a shared motif.' I forgot to extract ATTAC, TTAC, TAC, AC etc. These also count as submotifs. That needs to be fixed.

--- finding-a-shared-motif/finding-a-shared-motif.py
+++ finding-a-shared-motif/finding-a-shared-motif.py
@@ -39,16 +39,64 @@ from collections import defaultdict
 
 DNA_STRINGS = ['GATTACA', 'TAGACCA', 'ATACA']
 
-DNA_SUBSTRINGS_DICT = defaultdict(list)
+def dna_string_to_motifs(DNA_DATA = DNA_STRINGS):
 
-for ELEMENT in DNA_STRINGS:
-    CURRENT_MOTIF_SAMPLE = ELEMENT
-    #Creates a list of substrings of current motif sample
-    MOTIF_SAMPLE_SUBSTRINGS = []
-    for NUMBER in range(len(CURRENT_MOTIF_SAMPLE) + 1):
-        if NUMBER > 1:
-            DNA_SUBSTRINGS_DICT[CURRENT_MOTIF_SAMPLE].append(CURRENT_MOTIF_SAMPLE[0:NUMBER])
+    '''
+        Converts a DNA string into all combinations of motifs it can make. Returns a dictionary where keys represent DNA strings, and values represent
+        list of all possible motifs.
+        >>>DNA_STRINGS = ['GATTACA', 'TAGACCA', 'ATACA']
+        >>>dna_string_to_motifs(DNA_STRINGS)
+        >>>{'GATTACA': ['GA', 'GAT', 'GATT', 'GATTA', 'GATTAC', 'GATTACA'], 'TAGACCA': ['TA', 'TAG', 'TAGA', 'TAGAC', 'TAGACC', 'TAGACCA'], 'ATACA': ['AT', 'ATA', 'ATAC', 'ATACA']}
 
-print(dict(DNA_SUBSTRINGS_DICT))
+    '''
 
-#Checking if there is a motif in other DNA strings
+    DNA_SUBSTRINGS_DICT = defaultdict(list)
+    #For each element in a DNA_STRINGS creates a list of substrings ('sub-motifs').
+    for ELEMENT in DNA_DATA:
+
+        #These 'submotifs' can be of length 2 up-to length of an element because they have diferent length, and could be thus contained in each other.
+        for NUMBER in range(len(ELEMENT) + 1):
+            if NUMBER > 1:
+                DNA_SUBSTRINGS_DICT[ELEMENT].append(ELEMENT[0:NUMBER])
+
+    return dict(DNA_SUBSTRINGS_DICT)
+
+'''
+- Create a default dictionary where keys represent submotifs, and values represent number of times it occured
+- Go trough all elements in DNA_STRINGS list where list[-1] != current_element (we do not want to count anything that is already in that element
+- Switch to the next list in dictionary
+- After finishing -1 each count (because of self-count!) 
+'''
+
+DNA_STRING_SUBMOTIFS_DICT = dna_string_to_motifs()
+
+DNA_SUBMOTIFS_DICT = defaultdict(int)
+
+for DICT_VALUE in DNA_STRING_SUBMOTIFS_DICT.values():
+    CURRENT_LIST = DICT_VALUE
+    for CURRENT_LIST_VALUE in CURRENT_LIST:
+        for ELEMENT in DNA_STRINGS:
+            if CURRENT_LIST_VALUE in ELEMENT:
+                DNA_SUBMOTIFS_DICT[CURRENT_LIST_VALUE]+=1
+
+#print(list(DNA_STRING_SUBMOTIFS_DICT.keys()))
+#print(dict(DNA_SUBMOTIFS_DICT))
+
+#########################I forgot to extract ATTAC, TTAC, TAC, AC etc. These also count as submotifs. ##########################
+
+string_h = "GORAN"
+
+#print(string_h[-5:5])
+
+list_help = []
+
+for position in reversed(range(2, len(string_h))):
+    print(string_h[-position:len(string_h)])
+
+#This needs to be implemented into function dna_string_to_motifs()
+
+
+
+
+
+
