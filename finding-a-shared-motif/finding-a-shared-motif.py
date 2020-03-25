bf2ccbc674cdabac5b2fa5b95ea48463dff70a90
lobvh <71gordi@gmail.com>
2020-03-25 19:59:24 +0100

    Almost finished with 'Finding a shared motif'. Need to fix data preprocessing for input data.

--- finding-a-shared-motif/finding-a-shared-motif.py
+++ finding-a-shared-motif/finding-a-shared-motif.py
@@ -18,82 +18,93 @@ Sample output
 AC
 '''
 
-####Scrible code... Initial estimations
-
-# I will hold everything in a list since 'If multiple solutions exist, you may return any single solution.', and I can use max() with key=len on that list.
-
-lista_stringova = ["Bla", "KRAS", "JAPAAR"]
-
-# print(len(max(lista_stringova, key=len)))
-
-HELPING_LIST = []
-if "MOTIF" in 'EACH_DATA_STRING':
-    pass
-
-'''
-- Minimum size of a motif is 2
-- Make a list of substrings from one >Rosalind_x and treat everything else as a strings to search through them
-- Since each DNA string can possibly be different sizes check also if full DNA string is included
-'''
+from itertools import combinations
 from collections import defaultdict
 
+HELPING_STRING = "GORAN"
 DNA_STRINGS = ['GATTACA', 'TAGACCA', 'ATACA']
 
-def dna_string_to_motifs(DNA_DATA = DNA_STRINGS):
+def strings_to_substrings(strings=HELPING_STRING):
 
     '''
         Converts a DNA string into all combinations of motifs it can make. Returns a dictionary where keys represent DNA strings, and values represent
         list of all possible motifs.
-        >>>DNA_STRINGS = ['GATTACA', 'TAGACCA', 'ATACA']
+        >>>DNA_STRINGS = "GORAN"
         >>>dna_string_to_motifs(DNA_STRINGS)
-        >>>{'GATTACA': ['GA', 'GAT', 'GATT', 'GATTA', 'GATTAC', 'GATTACA'], 'TAGACCA': ['TA', 'TAG', 'TAGA', 'TAGAC', 'TAGACC', 'TAGACCA'], 'ATACA': ['AT', 'ATA', 'ATAC', 'ATACA']}
+        >>>{'GORAN': ['GO', 'GOR', 'GORA', 'GORAN', 'OR', 'ORA', 'ORAN', 'RA', 'RAN', 'AN']}
 
     '''
 
-    DNA_SUBSTRINGS_DICT = defaultdict(list)
-    #For each element in a DNA_STRINGS creates a list of substrings ('sub-motifs').
-    for ELEMENT in DNA_DATA:
+    #First line of defense if someone invokes this function using string as an input:
 
-        #These 'submotifs' can be of length 2 up-to length of an element because they have diferent length, and could be thus contained in each other.
-        for NUMBER in range(len(ELEMENT) + 1):
-            if NUMBER > 1:
-                DNA_SUBSTRINGS_DICT[ELEMENT].append(ELEMENT[0:NUMBER])
+    temporary_list = [element
+                      for element in strings]
+    strings_to_substrings_dict = defaultdict(list)
 
-    return dict(DNA_SUBSTRINGS_DICT)
+
+    for each_string in temporary_list:
+
+        all_combinations_of_substrings = [each_string[range_start:range_end]
+                                          for range_start, range_end in combinations(range(len(each_string) + 1), r=2)]
+        substrings_of_size_2_and_more = [substring
+                                         for substring in all_combinations_of_substrings
+                                         if len(substring) > 1]
+        strings_to_substrings_dict[each_string] = substrings_of_size_2_and_more
+
+    return dict(strings_to_substrings_dict)
+
+print(strings_to_substrings(DNA_STRINGS))
+
+def longest_common_motif(data = DNA_STRINGS):
+
+    '''
+        Counts the number of motif occurencies in a given set of strings. Returns most common motif (motif with highest number of counts).
+        >>> DNA_STRINGS = ['GATTACA', 'TAGACCA', 'ATACA']
+        >>> longest_common_motif(DNA_STRINGS)
+        >>> ('TA', 8)
+    '''
+
+    data_preprocessed = strings_to_substrings(data)
+    counting_dict = defaultdict(int)
+
+    #Accesing necessary elements for comparison
+    for each_element in data:
+        for value in data_preprocessed.values():
+            list_of_substrings = value
+            for substring in list_of_substrings:
+                if substring in each_element:
+                    counting_dict[substring] += 1
+
+    helping_dict = dict(counting_dict)
+
+    #Since we counted substrings in their own strings we have to -1 each count
+    helping_dict_refined = helping_dict
+
+    for key in helping_dict.keys():
+        helping_dict_refined[key] -= 1
+
+    max_occurent_motif = max(helping_dict_refined, key=helping_dict_refined.get)
+    return max_occurent_motif, helping_dict_refined[max_occurent_motif]
+
+
+print(longest_common_motif())
+
+
+with open('finding-a-shared-motif.txt') as txt_file:
+
+    read_data = []
+
+    for each_line in txt_file:
+        read_data.append(each_line)
+
 
 '''
-- Create a default dictionary where keys represent submotifs, and values represent number of times it occured
-- Go trough all elements in DNA_STRINGS list where list[-1] != current_element (we do not want to count anything that is already in that element
-- Switch to the next list in dictionary
-- After finishing -1 each count (because of self-count!) 
+-Need to create a dictionary where key, value pairs are something like >Rosalind_8323 : 'GTTCTATATTATACATCCAGGAAGATTTATTGGTGG'
 '''
 
-DNA_STRING_SUBMOTIFS_DICT = dna_string_to_motifs()
 
-DNA_SUBMOTIFS_DICT = defaultdict(int)
 
-for DICT_VALUE in DNA_STRING_SUBMOTIFS_DICT.values():
-    CURRENT_LIST = DICT_VALUE
-    for CURRENT_LIST_VALUE in CURRENT_LIST:
-        for ELEMENT in DNA_STRINGS:
-            if CURRENT_LIST_VALUE in ELEMENT:
-                DNA_SUBMOTIFS_DICT[CURRENT_LIST_VALUE]+=1
 
-#print(list(DNA_STRING_SUBMOTIFS_DICT.keys()))
-#print(dict(DNA_SUBMOTIFS_DICT))
-
-#########################I forgot to extract ATTAC, TTAC, TAC, AC etc. These also count as submotifs. ##########################
-
-string_h = "GORAN"
-
-#print(string_h[-5:5])
-
-list_help = []
-
-for position in reversed(range(2, len(string_h))):
-    print(string_h[-position:len(string_h)])
-
-#This needs to be implemented into function dna_string_to_motifs()
 
 
 
